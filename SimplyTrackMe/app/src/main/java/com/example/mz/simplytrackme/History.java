b9ebd32b3c0c50436b21c43123517190c8a2ef62
Unknown <michal.zwonek@gmail.com>
2017-05-06 15:07:58 +0200

    Added Android Project

---
+++ SimplyTrackMe/app/src/main/java/com/example/mz/simplytrackme/History.java
@@ -0,0 +1,13 @@
+package com.example.mz.simplytrackme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
---
+++ SimplyTrackMe/app/src/main/java/com/example/mz/simplytrackme/Main.java
@@ -0,0 +1,23 @@
+package com.example.mz.simplytrackme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Main extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void goToHistory(View view)
+    {
+        Intent intent = new Intent(this, History.class);
+
+        startActivity(intent);
+    }
+}
