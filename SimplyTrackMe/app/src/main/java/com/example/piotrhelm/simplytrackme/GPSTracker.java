bc4731c7e03fb40140c4cff7fbca59559d2e14f6
Unknown <michal.zwonek@gmail.com>
2017-05-07 20:05:00 +0200

    Ask for permission Android 6.0+ constraint
    Added permission query.

--- SimplyTrackMe/app/src/main/java/com/example/piotrhelm/simplytrackme/GPSTracker.java
+++ SimplyTrackMe/app/src/main/java/com/example/piotrhelm/simplytrackme/GPSTracker.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 /**
