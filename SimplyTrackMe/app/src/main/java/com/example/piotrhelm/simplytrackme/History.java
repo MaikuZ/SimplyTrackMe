32bdebf67b29158d2d23592096b94b3acdde288a
Unknown <helm.piotr@gmail.com>
2017-05-07 18:26:35 +0200

    Saving file

--- SimplyTrackMe/app/src/main/java/com/example/piotrhelm/simplytrackme/History.java
+++ SimplyTrackMe/app/src/main/java/com/example/piotrhelm/simplytrackme/History.java
@@ -1,7 +1,14 @@
 package com.example.piotrhelm.simplytrackme;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class History extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        String filename = "myfile";
+        String outputString = "Hello world!";
+
+        try {
+            FileOutputStream outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(outputString.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /*try {
+            FileInputStream inputStream = openFileInput(filename);
+            BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder total = new StringBuilder();
+            String line;
+            while ((line = r.readLine()) != null) {
+                total.append(line);
+            }
+            r.close();
+            inputStream.close();
+            Log.d("File", "File contents: " + total);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
     }
 }
