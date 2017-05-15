df50cda1dc3fcf202e190785cd8b9d2ad40ce3ee
Piotr Helm <helm.piotr@gmail.com>
2017-05-15 14:11:25 +0200

    Add maps

--- SimplyTrackMe/app/src/main/java/com/example/piotrhelm/simplytrackme/Start.java
+++ SimplyTrackMe/app/src/main/java/com/example/piotrhelm/simplytrackme/Start.java
@@ -9,6 +9,7 @@ import android.content.res.Configuration;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -118,5 +119,7 @@ public class Start extends AppCompatActivity {
         currentTrack.saveToFile(this);
         GPSUpdater.interrupt();
         finish();
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
