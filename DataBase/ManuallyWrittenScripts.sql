5e04353f19b177fa5c25f2e2067f448aca4a43d6
Unknown <helm.piotr@gmail.com>
2017-06-11 21:17:11 +0200

    Updated .sql script

--- DataBase/ManuallyWrittenScripts.sql
+++ DataBase/ManuallyWrittenScripts.sql
@@ -71,3 +71,51 @@ create or replace rule ranking_distance_alltime_do_nothing as on INSERT
 ----
 
 
+--Trigger user_session
+
+CREATE OR REPLACE FUNCTION delete_if_owner()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  IF (old.id_user = (SELECT id_owner FROM simplytrackme.sessions WHERE id_session = old.id_session)) THEN
+    DELETE FROM simplytrackme.sessions WHERE id_session = old.id_session;
+  END IF;
+  return null;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE trigger user_owner_delete after delete on simplytrackme.user_sessions
+for each row execute procedure delete_if_owner();
+
+--person distance
+CREATE OR REPLACE FUNCTION person_distance(interval, _name VARCHAR)
+  RETURNS numeric AS
+$$
+BEGIN
+  RETURN (SELECT
+    coalesce(sum(distance),0)
+  FROM simplytrackme.users
+    JOIN simplytrackme.user_sessions us ON simplytrackme.users.id_user = us.id_user
+    JOIN simplytrackme.sessions s ON us.id_session = s.id_session
+  WHERE current_date - s.begin_time <= $1 AND user_name = _name);
+END;
+$$
+LANGUAGE plpgsql;
+--ranking distance
+CREATE OR REPLACE FUNCTION ranking_distance(INTERVAL)
+  RETURNS TABLE(
+    _user_name VARCHAR,
+    distance   NUMERIC
+  ) AS
+$$
+BEGIN
+  RETURN QUERY
+  SELECT
+    user_name,
+    coalesce((SELECT person_distance($1, user_name)), 0)
+  FROM simplytrackme.users
+  order by 2 desc,1;
+END;
+$$
+LANGUAGE plpgsql;
