7434c78b480c3f79048c5f4deadf2bf73d781bee
Unknown <michal.zwonek@gmail.com>
2017-06-12 21:10:22 +0200

    Cleaned Start.java
    Cleaned and described Start.java

--- SimplyTrackMe/app/src/main/java/com/example/piotrhelm/simplytrackme/Start.java
+++ SimplyTrackMe/app/src/main/java/com/example/piotrhelm/simplytrackme/Start.java
@@ -30,15 +30,17 @@ import java.util.Calendar;
 public class Start extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private Track currentTrack = null;
     private Thread GPSUpdater;
-    private Marker currentmarker = null;
+    private Marker currentMarker = null;
+    private Track currentTrack = null;
     
     private int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
+
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         return;
-    }
+    }//Necessary
+
     private void updateTextView(String newText,TextView textView) {
         textView.setText(newText);
     }
@@ -50,24 +52,27 @@ public class Start extends FragmentActivity implements OnMapReadyCallback {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //This is code for loading mapFragment
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //This is the textView that show currentStats
         final TextView currentStats = (TextView)findViewById(R.id.textView);
 
+        //New instance of GPSTracker, a class that gives data from gps module.
         final GPSTracker tracker = new GPSTracker(this);
 
 
         if(!tracker.isGPSOn) {
             tracker.showSettingsAlert();
         }
-        // Here, thisActivity is the current activity
+
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
 
@@ -76,38 +81,31 @@ public class Start extends FragmentActivity implements OnMapReadyCallback {
                 // sees the explanation, try again to request the permission.
 
             } else {
-
                 // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
         int permissionCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
+        //We check if the permission for GPS was granted
+
         if(permissionCheck == PackageManager.PERMISSION_DENIED)
         {
             Toast.makeText(this,"No permission for gps. Please permit it.",Toast.LENGTH_SHORT).show();
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            //We stop it
+            updateTextView("You didn't allow the use of GPS.\nTo track your activity this application " +
+                    "needs the access to GPS.\nPlease allow it and then click the red button to go back to main Activity."
+            , currentStats);
             return;
         }
         tracker.getLocation();
-        /*
-        {///Alert with current location
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-            alertDialog.setTitle("GPS location");
-            alertDialog.setMessage("Current Lat: " + tracker.getLat() + "Current Lon: " + tracker.getLon());
-            alertDialog.show();
-        }
-        */
-        currentTrack = new Track();
+        currentTrack = new Track();//createNewTrack
         currentTrack.setOwner(new Person(
                 PreferenceManager.getDefaultSharedPreferences(this).getString("person_name", "none"),
                 PreferenceManager.getDefaultSharedPreferences(this).getString("user_name", "none"),
@@ -130,16 +128,15 @@ public class Start extends FragmentActivity implements OnMapReadyCallback {
                                 if(tracker.isPossibleGetLocation())
                                 if(tracker.getLocation() != null) {
                                     currentTrack.addNode(tracker.getLat(), tracker.getLon(), Calendar.getInstance().getTime().getTime(), tracker.getAltitude());
-                                    updateTextView("Current Distance: " +
-                                            new DecimalFormat("#0.0").format(currentTrack.getTotalDistance() / 1000) + " km" + "\n" +
-                                            // "Current Location: " + tracker.getUpdatedLocation() + "\n" +
-                                            "Current altitude: " + tracker.getAltitude() + "\n" +
-                                            "Elapsed time: " + (Calendar.getInstance().getTime().getTime() - currentTrack.getStart_date()) / 1000 / 60 + " minutes" + "\n" +
-                                            "Average speed: " + new DecimalFormat("#0.0").format((currentTrack.getTotalDistance() / 1000) / (Calendar.getInstance().getTime().getTime() - currentTrack.getStart_date()) * 1000 * 60 * 60) + " km/h", currentStats
-                                    );
+                                    updateTextView("Current Distance: "
+                                            + new DecimalFormat("#0.0").format(currentTrack.getTotalDistance() / 1000) + " km" + "\n"
+                                            + "Current altitude: " + tracker.getAltitude() + "\n"
+                                            + "Elapsed time: " + (Calendar.getInstance().getTime().getTime() - currentTrack.getStart_date()) / 1000 / 60 + " minutes" + "\n"
+                                            + "Average speed: " + new DecimalFormat("#0.0").format((currentTrack.getTotalDistance() / 1000) / (Calendar.getInstance().getTime().getTime() - currentTrack.getStart_date()) * 1000 * 60 * 60) + " km/h"
+                                            , currentStats);
+                                    if(currentTrack.getList().size() >= 1)//Update the map only if there is at least one node
+                                        onUptadeMap();
                                 }
-                                if(currentTrack.getList().size() >= 1)
-                                    onUptadeMap();
                             }
                         });
                     }
@@ -147,26 +144,32 @@ public class Start extends FragmentActivity implements OnMapReadyCallback {
                 }
             }
         };
-
         GPSUpdater.start();
     }
 
+    //This is executed each time the list in currentTrack is updated
+    //I.E. there is a new node => we need to add the line between last 2 nodes
     public void onUptadeMap(){
         ArrayList<Track.Node> Temp = currentTrack.getList();
         ArrayList<LatLng> List = new ArrayList<>();
 
-        for(int i = 0; i < Temp.size(); i++) {
+        //We add at most 2 nodes to the list. 2 last nodes.
+        //This is different than in MapsActivity where we add all nodes at once, because
+        //this function is called each update in the currentTrack list is made
+        for(int i = Temp.size()-2; i < Temp.size(); i++) {
+            if(i < 0)
+                continue;
             LatLng e = new LatLng(Temp.get(i).getLat(), Temp.get(i).getLon());
             List.add(e);
         }
 
         LatLng marker = new LatLng(currentTrack.getLast().getLat(),currentTrack.getLast().getLon());
-        if(currentmarker != null)
-        {
-            currentmarker.remove();
-            currentmarker = null;
+        if(currentMarker != null) {///Remove last marker
+            currentMarker.remove();
+            currentMarker = null;
         }
-        currentmarker = mMap.addMarker(new MarkerOptions().position(marker).title("You're here"));
+        currentMarker = mMap.addMarker(new MarkerOptions().position(marker).title("You're here."));
+
 
         PolylineOptions polylineOptions = new PolylineOptions();
 
@@ -187,12 +190,13 @@ public class Start extends FragmentActivity implements OnMapReadyCallback {
             finish();
             return;
         }
+
         currentTrack.setEnd_date(currentTrack.getStart_date() + currentTrack.getLast().getTime_elapsed());
         currentTrack.saveToFile(this);
         GPSUpdater.interrupt();
 
         Intent myIntent = new Intent(view.getContext(), trackDetailsView.class);
-        myIntent.putExtra("fileName", currentTrack.getID() + ".json");
+        myIntent.putExtra("fileName", currentTrack.getID() + ".json");///pass filename to trackDetailsView activity
         startActivityForResult(myIntent, 0);
         finish();
     }
@@ -200,11 +204,5 @@ public class Start extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        /*LatLng marker = new LatLng(new GPSTracker(this).getLat(),new GPSTracker(this).getLon());
-        mMap.addMarker(new MarkerOptions().position(marker).title("Tu jesteś!!!"));*/
-
-
     }
 }
