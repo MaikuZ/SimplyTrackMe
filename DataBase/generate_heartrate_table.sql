a433a6953109dcc1a94c84ec73b7d6dd4ab68358
Wiktor Daniec <wiktor.prodg.daniec@gmail.com>
2017-06-13 22:12:45 +0200

    added two generators and divided psql triggers,rules,functions

---
+++ DataBase/generate_heartrate_table.sql
@@ -0,0 +1,67 @@
+-- dodac check, ze hearthrate w przedziale (50,220)
+-- dodac check, ze srednia predkosc biegania w przedziale (0,20)
+
+-- wypelnianie dla biegania
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (0.0,5.0,0,70);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (5.0,6.0,0,80);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (6.0,7.0,0,90);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (7.0,8.0,0,100);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (8.0,9.0,0,110);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (9.0,10.0,0,130);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (10.0,11.0,0,150);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (11.0,12.0,0,160);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (12.0,13.0,0,170);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (13.5,14.0,0,180);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (14.0,14.5,0,185);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (14.5,20.0,0,190);
+
+-- dodac check, ze srednia predkosc na rolkach jest w przedziale(0,45)
+
+-- wypelnienie dla rolek
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (0.0,6.0,3,80);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (6.0,7.0,3,80);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (7.0,9.0,3,90);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (9.0,10.0,3,100);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (10.0,11.0,3,110);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (11.0,12.0,3,120);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (12.0,14.0,3,125);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (14.0,16.0,3,135);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (16.0,18.0,3,145);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (18.0,20.0,3,155);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (20.0,21.0,3,160);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (21.0,22.0,3,170);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (22.0,25.0,3,175);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (25.0,30.0,3,180);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (30.0,45.0,3,190);
+
+-- dodac check, ze srednia predkosc dla rowerow jest w przedziale (0,50)
+
+-- wypelnienie dla roweru
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (0.0,5.0,1,70)
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (5.0,10.0,1,80);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (10.0,13.0,1,90);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (13.0,15.0,1,100);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (15.0,18.0,1,110);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (18.0,20.0,1,120);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (20.0,21.0,1,130);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (21.0,23.0,1,140);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (23.0,25.0,1,150);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (25.0,27.0,1,160);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (27.0,29.0,1,170);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (29.0,31.0,1,175);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (31.0,33.0,1,180);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (33.0,34.0,1,185);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (34.0,35.0,1,190);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (35.0,50.0,1,195);
+
+-- dodach check, ze srednia predkosc dla nart to max 120
+
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (0.0,5.0,4,60);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (5.0,20.0,4,100);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (20.0,40.0,4,140);
+insert into simplytrackme.type_heartrate_intervals (minimum_speed, maximum_speed, id_type, heartrate) values (40.0,120.0,4,160);
+
+
+-- plywanie dzieje bez komorki, wiec jest troche problem ze zdeterminowaniem predkosci
+insert into simplytackme.type_heartrate_intervales (minimum_speed, maximum_speed, id_type, heartrate) values
+(0.0,5.0,2,140);
